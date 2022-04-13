7704a978349e9a9997c931da6461f7b17bcb054a
unknown <lsr488@gmail.com>
2022-04-13 09:24:24 -0400

    Add React Toastify for error handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/NavBar'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
@@ -11,17 +13,19 @@ import ForgotPassword from './pages/ForgotPassword'
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Explore />} />
-        <Route path='/offers' element={<Offers />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-      </Routes>
-      <Navbar />
-    </Router>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+
+      <ToastContainer />
     </>
   );
 }
