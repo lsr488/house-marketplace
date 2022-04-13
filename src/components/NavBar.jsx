975c3f9aa76cd8ed4e1438bd58d58049f40e06b1
unknown <lsr488@gmail.com>
2022-04-13 08:17:02 -0400

    Initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import {useNavigate, useLocation} from 'react-router-dom'
+import {ReactComponent as OfferIcon} from '../assets/svg/localOfferIcon.svg'
+import {ReactComponent as ExploreIcon} from '../assets/svg/exploreIcon.svg'
+import {ReactComponent as PersonOutlineIcon} from '../assets/svg/personOutlineIcon.svg'
+
+function Navbar() {
+	return (
+		<footer className="navbar">
+			<nav className="navbarBav">
+				<ul className="navbarListItems">
+					<li className="navbarListItems">
+						
+					</li>
+				</ul>
+			</nav>
+		</footer>
+	)
+}
+
+export default Navbar
