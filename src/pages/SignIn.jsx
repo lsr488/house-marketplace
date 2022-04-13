251deda5a6b555218ebfb4d233526fb60b946411
unknown <lsr488@gmail.com>
2022-04-13 09:16:26 -0400

    User sign in with email and password on submit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visbilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -19,6 +20,22 @@ function SignIn() {
       [e.target.id]: e.target.value,
     }))
   }
+  
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      const auth = getAuth()
+
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+      if(userCredential.user) {
+        navigate('/')
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
 
   return (
     <>
@@ -29,7 +46,7 @@ function SignIn() {
           </p>
         </header>
 
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
 
             <div className="passwordInputDiv">
