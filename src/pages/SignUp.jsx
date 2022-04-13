fa0e1394972165557617df5d334c7ba2f23deeda
unknown <lsr488@gmail.com>
2022-04-13 08:57:53 -0400

    Register user to Firebase

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,7 @@
 import {useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import {db} from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visbilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +23,26 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      const auth = getAuth()
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name
+      })
+
+      navigate('/')
+    } catch(error) {
+
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -30,7 +52,7 @@ function SignUp() {
           </p>
         </header>
 
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="text" className="nameInput" placeholder="Name" id="name" value={name} onChange={onChange} />
 
             <input type="email" className="emailInput" placeholder="Email" id="email" value={email} onChange={onChange} />
