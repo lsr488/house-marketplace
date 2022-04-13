65c6dd1cbbd3e8549830bf97da398a431110f03f
unknown <lsr488@gmail.com>
2022-04-13 08:29:19 -0400

    Styling applied to active route

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,21 +7,27 @@ function Navbar() {
 	const navigate = useNavigate()
 	const location = useLocation()
 
+	const pathMatchRoute = (route) => {
+		if(route == location.pathname) {
+			return true
+		}
+	}
+
 	return (
 		<footer className="navbar">
 			<nav className="navbarNav">
 				<ul className="navbarListItems">
 					<li className="navbarListItems" onClick={() => navigate('/')}>
-						<ExploreIcon fill="#2c2c2c" width="36px" height="36px"/>
-						<p>Explore</p>
+						<ExploreIcon fill={pathMatchRoute('/') ? "#2c2c2c" : "#8f8f8f"} width="36px" height="36px"/>
+						<p className={pathMatchRoute('/') ? "navbarListItemNameActive" : "navbarListItemName"}>Explore</p>
 					</li>
 					<li className="navbarListItems" onClick={() => navigate('/offers')}>
-						<OfferIcon fill="#2c2c2c" width="36px" height="36px"/>
-						<p>Offer</p>
+						<OfferIcon fill={pathMatchRoute('/offers') ? "#2c2c2c" : "#8f8f8f"} wwidth="36px" height="36px"/>
+						<p className={pathMatchRoute('/offers') ? "navbarListItemNameActive" : "navbarListItemName"}>Offers</p>
 					</li>
 					<li className="navbarListItems" onClick={() => navigate('/profile')}>
-						<PersonOutlineIcon fill="#2c2c2c" width="36px" height="36px"/>
-						<p>Profile</p>
+						<PersonOutlineIcon fill={pathMatchRoute('/profile') ? "#2c2c2c" : "#8f8f8f"} wwidth="36px" height="36px"/>
+						<p className={pathMatchRoute('/profile') ? "navbarListItemNameActive" : "navbarListItemName"}>Profile</p>
 					</li>
 				</ul>
 			</nav>
