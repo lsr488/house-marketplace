e49f443c99211a06b4fad28f986d21c7713a31b6
unknown <lsr488@gmail.com>
2022-04-18 08:37:49 -0400

    Fixed export and property name

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,13 +1,13 @@
 import {useEffect, useState} from 'react'
-import {getAuth, onAuthStateChange} from 'firebase/auth'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
 
-export function useAuthStatus = () => {
+export const useAuthStatus = () => {
 	const [loggedIn, setLoggedIn] = useState(false)
 	const [checkingStatus, setCheckingStatus] = useState(true)
 
 	useEffect(() => {
 		const auth = getAuth()
-		onAuthStateChange(auth, (user) => {
+		onAuthStateChanged(auth, (user) => {
 			if(user) {
 				setLoggedIn(true)
 			}
