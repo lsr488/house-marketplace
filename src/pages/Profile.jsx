ae720d92ed7edac4d6ce9d840bc6fee02f627181
unknown <lsr488@gmail.com>
2022-04-20 08:47:53 -0400

    Add link to user listings

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,10 @@ import {useState} from 'react'
 import {getAuth, updateProfile} from 'firebase/auth'
 import {updateDoc, doc} from 'firebase/firestore'
 import {db} from '../firebase.config'
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 import {toast} from 'react-toastify'
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
 
 function Profile() {
   const auth = getAuth()
@@ -75,9 +77,16 @@ function Profile() {
             <input type="text" id="name" className={!changeDetails ? 'profileName' : 'profileNameActive'} disabled={!changeDetails} value={name} onChange={onChange} />
 
             <input type="text" id="email" className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange} />
-
           </form>
         </div>
+
+      {/* user listings */}
+      <Link to='/create-listing' className='createListing'>
+        <img src={homeIcon} alt="home" />
+        <p>Sell or rent your home</p>
+        <img src={arrowRight} alt="arrow right image" />
+      </Link>
+
       </main>
     </div>
   )
