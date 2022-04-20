40cb84cbc7a6ae050c8c06cbbbf87ff5d2108411
unknown <lsr488@gmail.com>
2022-04-20 07:37:31 -0400

    Fetch and set listings data

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,58 @@
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {toast} from 'react-toastify'
+import Spinner from '../components/Spinner'
 
 function Category() {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // get reference
+        const listingsRef = collection(db, 'listings')
+
+        // create query
+        const q = query(
+          listingsRef, 
+          where('type', '==', params.categoryName), 
+          orderBy('timestamp', 'desc'), 
+          limit(10)
+        )
+
+        // execute query
+        const querySnapshot = await getDocs(q)
+
+        // iterate through querySnapshot
+        const listings = []
+        querySnapshot.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        })
+
+        // set listings
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
+    }
+
+    fetchListings()
+  }, [])
+
   return (
     <div>
      <h1>Category</h1>
     </div>
-  );
+  )
 }
 
-export default Category;
+export default Category
