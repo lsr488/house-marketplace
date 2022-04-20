03e091ef291a38525406e62904f19651b5bede9e
unknown <lsr488@gmail.com>
2022-04-20 07:54:26 -0400

    Initial commit

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,14 @@
+import {Link} 'react-router-dom'
+import {ReactComponent as DeleteIcon} from '../assets/svg/deleteIcon.svg'
+import bedIcon from '../assets/svg/bedIcon.svg'
+import bathtubIcon from '../assets/svg/bathtubIcon.svg'
+
+function ListingItem() {
+  return (
+    <div>
+     <h1>ListingItem</h1>
+    </div>
+  );
+}
+
+export default ListingItem
