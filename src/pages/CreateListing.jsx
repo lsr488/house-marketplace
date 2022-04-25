d7acb4b21121e8be7836b95a456875e0152d9311
unknown <lsr488@gmail.com>
2022-04-25 09:35:47 -0400

    Save data to firestore db

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
+import {addDoc, collection, serverTimestamp} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
@@ -138,9 +139,25 @@ function CreateListing() {
 			return
 		})
 
-		console.log(imgUrls)
+		const formDataCopy = {
+			...formData,
+			imgUrls,
+			geolocation,
+			timestamp: serverTimestamp()
+		}
+
+		delete formDataCopy.images
+		delete formDataCopy.address
+		location && (formDataCopy.location = location)
+		!formDataCopy.offer && delete formDataCopy.discountedPrice
+
+		const docRef = await addDoc(collection(db, 'listings'), formDataCopy)
 
 		setLoading(false)
+
+		toast.success('Listing saved')
+
+		navigate(`/category/${formDataCopy.type}/${docRef.id}`)
 	}
 
 	const onMutate = (e) => {
