2dfbe6342989cabbc9fae37d06dd75a7152d4dbe
unknown <lsr488@gmail.com>
2022-04-25 08:54:12 -0400

    Geolocation disabled and location data saved

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,10 +1,11 @@
 import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
-	const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+	const [geolocationEnabled, setGeolocationEnabled] = useState(false)
 	const [loading, setLoading] = useState(false)
 
 	const [formData, setFormData] = useState({
@@ -45,9 +46,50 @@ function CreateListing() {
 		}
 	}, [isMounted])
 
-	const onSubmit = (e) => {
+	const onSubmit = async (e) => {
 		e.preventDefault();
-		console.log(formData)
+
+		setLoading(true)
+
+
+		if(offer && discountedPrice >= regularPrice) {
+			setLoading(false)
+			toast.error('Discounted price needs to be less than the regular price.')
+			return
+		}
+
+		if(images.length > 6) {
+			setLoading(false)
+			toast.error('You can only upload 6 images.')
+			return
+		}
+
+		let geolocation = {}
+		let location = {}
+		// if going into production, need to hide API Key	
+
+		if(geolocationEnabled) {
+			const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
+
+			const data = response.json()
+			console.log(data)
+			geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+			geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+
+			location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address
+
+			if(location === 'undefined' || location.includes('undefined')) {
+				setLoading(false)
+				toast.error('Address is incorrect')
+				return
+			}
+		} else {
+			geolocation.lat = latitude
+			geolocation.lng = longitude
+			location = address
+		}
+
+		setLoading(false)
 	}
 
 	const onMutate = (e) => {
