fc3d19e7755894f2fbe01ce194a0b58bc06f728a
unknown <lsr488@gmail.com>
2022-04-25 08:19:07 -0400

    Add more fields to form

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -185,6 +185,107 @@ function CreateListing() {
 						onChange={onMutate}
 						required
 					/>
+
+					{!geolocationEnabled && (
+            <div className='formLatLng flex'>
+              <div>
+                <label className='formLabel'>Latitude</label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='latitude'
+                  value={latitude}
+                  onChange={onMutate}
+                  required
+                />
+              </div>
+              <div>
+                <label className='formLabel'>Longitude</label>
+                <input
+                  className='formInputSmall'
+                  type='number'
+                  id='longitude'
+                  value={longitude}
+                  onChange={onMutate}
+                  required
+                />
+              </div>
+            </div>
+          )}
+
+					<label className='formLabel'>Offer</label>
+          <div className='formButtons'>
+            <button
+              className={offer ? 'formButtonActive' : 'formButton'}
+              type='button'
+              id='offer'
+              value={true}
+              onClick={onMutate}
+            >
+              Yes
+            </button>
+            <button
+              className={
+                !offer && offer !== null ? 'formButtonActive' : 'formButton'
+              }
+              type='button'
+              id='offer'
+              value={false}
+              onClick={onMutate}
+            >
+              No
+            </button>
+          </div>
+
+					<label className='formLabel'>Regular Price</label>
+	          <div className='formPriceDiv'>
+	            <input
+	              className='formInputSmall'
+	              type='number'
+	              id='regularPrice'
+	              value={regularPrice}
+	              onChange={onMutate}
+	              min='50'
+	              max='750000000'
+	              required
+	            />
+	            {type === 'rent' && <p className='formPriceText'>$ / Month</p>}
+	          </div>
+
+	          {offer && (
+	            <>
+	              <label className='formLabel'>Discounted Price</label>
+	              <input
+	                className='formInputSmall'
+	                type='number'
+	                id='discountedPrice'
+	                value={discountedPrice}
+	                onChange={onMutate}
+	                min='50'
+	                max='750000000'
+	                required={offer}
+	              />
+	            </>
+	          )}
+
+	          <label className='formLabel'>Images</label>
+	          	<p className='imagesInfo'>
+		            The first image will be the cover (max 6).
+	       			</p>
+		          <input
+		            className='formInputFile'
+		            type='file'
+		            id='images'
+		            onChange={onMutate}
+		            max='6'
+		            accept='.jpg,.png,.jpeg'
+		            multiple
+		            required
+		          />
+          <button type='submit' className='primaryButton createListingButton'>
+            Create Listing
+          </button>
+
 				</form>
 			</main>
 	</div>
