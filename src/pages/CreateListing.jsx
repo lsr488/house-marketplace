5727f4fcc75ee67e1e6162273b89c66ee8c728b1
unknown <lsr488@gmail.com>
2022-04-25 08:59:28 -0400

    Add uuid and firebase storage

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,10 @@
 import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage'
+import {db} from '../firebase.config'
 import {useNavigate} from 'react-router-dom'
 import {toast} from 'react-toastify'
+import {v4 as uuidv4} from 'uuid'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
