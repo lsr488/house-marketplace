d69e3b141f551ab9c1fff3a8d162292fed3ceed4
unknown <lsr488@gmail.com>
2022-04-25 09:16:50 -0400

    Add images to firestore db storage

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -92,6 +92,54 @@ function CreateListing() {
 			location = address
 		}
 
+		// store image in firebase
+		const storeImage = async (image) => {
+			return new Promise((resolve, reject) => {
+				const storage = getStorage()
+				const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+
+				const storageRef = ref(storage, 'images/' + fileName)
+
+				const uploadTask = uploadBytesResumable(storageRef, image)
+
+
+				uploadTask.on('state_changed', 
+			  (snapshot) => {
+			    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+			    console.log('Upload is ' + progress + '% done');
+			    switch (snapshot.state) {
+			      case 'paused':
+			        console.log('Upload is paused');
+			        break;
+			      case 'running':
+			        console.log('Upload is running');
+			        break;
+			    }
+			  }, 
+			  (error) => {
+			    reject(error)
+			  }, 
+			  () => {
+			    // Handle successful uploads on complete
+			    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+			    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+			      resolve(downloadURL);
+			    });
+			  }
+			);
+			})
+		}
+
+		const imgUrls = await Promise.all(
+			[...images].map((img) => storeImage(img))
+		).catch(() => {
+			setLoading(false)
+			toast.error('Images not uploaded')
+			return
+		})
+
+		console.log(imgUrls)
+
 		setLoading(false)
 	}
 
