ab2455ac7d7e82e8acfcca8d3fa9b78751475b94
unknown <lsr488@gmail.com>
2022-04-26 09:50:21 -0400

    Add contact landlord

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
 import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 
 function App() {
@@ -34,6 +35,8 @@ function App() {
           <Route path='/create-listing' element={<CreateListing />} />
 
           <Route path='/category/:categoryName/:listingId' element={<Listing />} />
+
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
